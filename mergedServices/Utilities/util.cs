9101fa1f48d34804d167933275bd40a38883f9ac
sherifkandeel <sherifkandel4@gmail.com>
2012-06-30 18:36:17 +0200

    getlabel is now alot faster
    -I now cache the labels got in a dictionary to check it before making a
    query
    -added new predicatesToExclude

--- mergedServices/Utilities/util.cs
+++ mergedServices/Utilities/util.cs
@@ -175,59 +175,121 @@ namespace mergedServices
             return false;
         }
 
+        //private static HashSet<KeyValuePair<string, string>> gottenlabels = new HashSet<KeyValuePair<string, string>>();
+        private static Dictionary<string, string> gottenlabels = new Dictionary<string, string>();
+        private static void addtohashset(string URI, string label)
+        {
+            if (gottenlabels.Count > 30000)
+            {
+                try
+                {
+                    gottenlabels.ToList().RemoveRange(0, 1000);
+                }
+                catch
+                {
+                    gottenlabels = new Dictionary<string, string>();
+                }
+
+            }
+            else
+            {
+
+                try
+                {
+                    if (!gottenlabels.ContainsKey(URI))
+                        gottenlabels.Add(URI, label);
+                    //gottenlabels.Add(URI, label);
+                }
+                catch
+                {
+                }
+            }
+        }
         public static string getLabel(string URI)
         {
             //if the string is empty return it as it is
             if (URI.Length == 0)
                 return URI;
-			//in case this is not a valid uri
-			if (!Uri.IsWellFormedUriString(URI, UriKind.Absolute))
-				return URI;
-            //at least best one for now
-            URI = Uri.EscapeUriString(URI);
-            //SparqlRemoteEndpoint endpoint = new SparqlRemoteEndpoint(new Uri("http://localhost:8890/sparql"));
-            string query = "select * where {<" + URI + "> <http://www.w3.org/2000/01/rdf-schema#label> ?obj}";
-            //SparqlResultSet results = endpoint.QueryWithResultSet(query);
-            SparqlResultSet results = Request.RequestWithHTTP(query);
-            //if there's no results from the first query, we will try to get the name 
-            if (results.Count < 1)
-            {
-                string name_query = "select * where {<" + URI + "> <http://xmlns.com/foaf/0.1/name> ?obj}";
-                //results = endpoint.QueryWithResultSet(name_query);
-                results = Request.RequestWithHTTP(name_query);
-
-                //if there's no result from the second query
-                //get the name after the /
-                if (results.Count < 1)
-                {
-                    string toreturn = new string(URI.ToCharArray().Reverse().ToArray());//URI.Reverse().ToString();                    
-                    toreturn = toreturn.Remove(toreturn.IndexOf("/"));
-                    toreturn = new string(toreturn.ToCharArray().Reverse().ToArray());
-                    toreturn = toreturn.Replace("_", " ");
-                    //TODO : get back the encoding
-                    toreturn = toreturn.Trim();
-                    //This part to return the one after the #
-                    if (toreturn.Contains("#"))
-                        return toreturn.Substring(toreturn.IndexOf("#"));
-                    if (toreturn.Length > 0)
-                        return toreturn;
-                    //this one is to return if empty
-                    else
-                        return URI;
-
-                }
-                else
-                {
-                    //returning
-                    return ((LiteralNode)results[0].Value("obj")).Value;
-                }
-            }
             else
             {
-                //returning it
-                return ((LiteralNode)results[0].Value("obj")).Value;
-            }
+                //gottenlabels.Select(e=>e
+                //string temp = gottenlabels.Select(e=>e.Key.Equals(URI)).Value;
+                string temp = "";
+                if (gottenlabels.TryGetValue(URI, out temp))
+                    return temp;
 
+                else
+                {
+                    //at least best one for now
+                    URI = Uri.EscapeUriString(URI);
+                    //SparqlRemoteEndpoint endpoint = new SparqlRemoteEndpoint(new Uri("http://localhost:8890/sparql"));
+                    string query = "select * where {<" + URI + "> <http://www.w3.org/2000/01/rdf-schema#label> ?obj}";
+                    //SparqlResultSet results = endpoint.QueryWithResultSet(query);
+                    SparqlResultSet results = Request.RequestWithHTTP(query);
+                    //if there's no results from the first query, we will try to get the name 
+                    if (results.Count < 1)
+                    {
+                        string name_query = "select * where {<" + URI + "> <http://xmlns.com/foaf/0.1/name> ?obj}";
+                        //results = endpoint.QueryWithResultSet(name_query);
+                        results = Request.RequestWithHTTP(name_query);
+
+                        //if there's no result from the second query
+                        //get the name after the /
+                        if (results.Count < 1)
+                        {
+                            string toreturn = new string(URI.ToCharArray().Reverse().ToArray());//URI.Reverse().ToString();                    
+                            toreturn = toreturn.Remove(toreturn.IndexOf("/"));
+                            toreturn = new string(toreturn.ToCharArray().Reverse().ToArray());
+                            toreturn = toreturn.Replace("_", " ");
+                            //TODO : get back the encoding
+                            toreturn = toreturn.Trim();
+                            //This part to return the one after the #
+                            if (toreturn.Contains("#"))
+                            {
+                                //adding to the hashset
+                                addtohashset(URI, toreturn.Substring(toreturn.IndexOf("#")));
+
+                                //return
+                                return toreturn.Substring(toreturn.IndexOf("#"));
+                            }
+                            if (toreturn.Length > 0)
+                            {
+                                //adding to the hashset
+                                addtohashset(URI, toreturn);
+
+                                //return
+                                return toreturn;
+                            }
+                            //this one is to return if empty
+                            else
+                            {
+                                //adding to the hashset
+                                addtohashset(URI, URI);
+
+                                //returning
+                                return URI;
+                            }
+
+                        }
+                        else
+                        {
+                            //adding to the hashset
+                            addtohashset(URI, ((LiteralNode)results[0].Value("obj")).Value);
+
+                            //returning
+                            return ((LiteralNode)results[0].Value("obj")).Value;
+                        }
+                    }
+                    else
+                    {
+                        //adding to the hashset
+                        addtohashset(URI, ((LiteralNode)results[0].Value("obj")).Value);
+
+                        //returning it
+                        return ((LiteralNode)results[0].Value("obj")).Value;
+                    }
+                }
+            }
         }
 		
         /// <summary>
